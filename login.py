452ca229f996b41d1eb9625c9a7ed133f8741e1a
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-04-21 23:03:28 -0400

    I created labels and variables for login information.

--- login.py
+++ login.py
@@ -3,6 +3,14 @@ from tkinter import ttk
 root = Tk()
 root.geometry('500x300')
 
-Label(root, text ="Gym Barcode", font='arial 15 bold')
+Label(root, text ="Gym Barcode", font='arial 15 bold').grid(row=0, column=3)
+
+username = Label(root, text='Username')
+password = Label(root, text='Password')
+barcode = Label(root, text='Barcode')
+
+username.grid(row=1,column=2)
+password.grid(row=2,column=2)
+barcode.grid(row=3,column=2)
 
 root.mainloop()
