0ac2f229ef3862fd4c076534f4e96406a8506ad4
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-04-25 11:00:49 -0400

    Created the labels and positions for username and password

--- login.py
+++ login.py
@@ -4,7 +4,14 @@ root = Tk()
 root.geometry('500x300')
 
 # Title of the Website 
-Label(root, text ="              Gym Barcode               ", font='arial 15 bold').grid(row=0, column=3)
+Label(root, text ="              Gym Barcode               ", font='arial 15 bold').grid(row=0, column=5)
 
+# Labels of entrys for login 
+username = Label(root, text= '  Username  ')
+password = Label(root, text= '  Password ')
+
+# Label postions 
+username.grid(row=1, column=5)
+password.grid(row=2, column=5)
 
 root.mainloop()
