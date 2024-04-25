04536833600f9d588ac7533d2001ceff423fb234
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-04-25 10:35:24 -0400

    Created same title for the website

--- login.py
+++ login.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 root = Tk()
 root.geometry('500x300')
 
+# Title of the Website 
+Label(root, text ="              Gym Barcode               ", font='arial 15 bold').grid(row=0, column=3)
 
 
 root.mainloop()
