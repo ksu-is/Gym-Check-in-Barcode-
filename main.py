592a57e7a6abeeca9eba0f4b90d096e455c7ab44
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-04-26 14:43:20 -0400

    created title of main window

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from tkinter import ttk
 root = Tk()
 root.geometry('500x300')
 
-
+#Title of the main window
 Label(root, text='              Welcome!', font='arial 15 bold').grid(row=0,column=25)
+
+
 
 
 root.mainloop()
