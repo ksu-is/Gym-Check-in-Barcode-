eca9949cb930d8db624fcd1fb600d22daf41845e
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-04-26 14:33:33 -0400

    created title of main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tkinter import * 
 from tkinter import ttk
-root = tk()
+root = Tk()
 root.geometry('500x300')
 
+
+Label(root, text='              Welcome!', font='arial 15 bold').grid(row=0,column=25)
+
+
+root.mainloop()
