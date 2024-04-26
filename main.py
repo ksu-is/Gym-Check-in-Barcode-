4796252261e5a60216458f46a52a89b871854ec4
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-04-26 14:46:07 -0400

    created label

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ root = Tk()
 root.geometry('500x300')
 
 #Title of the main window
-Label(root, text='              Welcome!', font='arial 15 bold').grid(row=0,column=25)
+Label(root, text='              Welcome!', font='arial 15 bold').grid(row=0,column=10)
+
+Label(root,text= 'Scan barcode below to sign in', font= 'arial 12').grid(row=1, column=3)
 
 
 
