2362edccaa26bcf9346b21c02d5c63794bbfdb66
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-05-02 12:37:03 -0400

    Edited function getvals() to set up storing username, password and barcode in login.py and main.py

--- createaccount.py
+++ createaccount.py
@@ -3,8 +3,17 @@ from tkinter import ttk
 root = Tk()
 root.geometry('500x300')
 
+# Save username, password and barcode to file
+# Create a file and open it in write mode
 def getvals():
-    print("Accepted")
+    username = usernamevalue.get()
+    password = passwordvalue.get()
+    barcode = barcodevalue.get() 
+    new_account = open('account.txt','w')
+    new_account.write(username + "\n")
+    new_account.write(password + "\n")
+    new_account.write(barcode)
+    print("Account created")
 
 #Title of the website
 Label(root, text ="              Gym Barcode              ", font='arial 15 bold').grid(row=0, column=3)
@@ -44,6 +53,7 @@ barcodeentry.grid(row=5, column=3)
 
 # Create Account button 
 Button(text="Create Account", command=getvals).grid(row=7,column=3)
+
 
 root.mainloop()
 
