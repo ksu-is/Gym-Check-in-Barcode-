1cdf146fc1af0f2126f25ade87da7a1483d47f6f
Iveth Betancourt <ibetanc1@students.kennesaw.edu>
2024-05-02 13:07:42 -0400

    created function login() to save username, password and barcode to main.py

--- login.py
+++ login.py
@@ -3,8 +3,24 @@ from tkinter import ttk
 root = Tk()
 root.geometry('500x300')
 
-def getvals():
-    print("Welcome")
+# Open file as read-on;y
+# .strip() to remove the whitespace 
+# Save username, password and barcode to main.py
+def login():
+    new_account = open('account.txt', 'r')
+    savedusername = new_account.readline().strip()
+    savedpassword = new_account.readline().strip()
+    savedbarcode = new_account.readline().strip()
+    
+    if username == savedusername and password == savedpassword and barcode == savedbarcode: 
+        print("Success")
+        return True 
+    else: 
+        print("Try again")
+        return False
+if __name__ == '__main__': 
+    login()
+    
 
 # Title of the Website 
 Label(root, text ="              Gym Barcode               ", font='arial 15 bold').grid(row=0, column=5)
@@ -29,7 +45,7 @@ usernameentry.grid (row=1, column= 5)
 passwordentry.grid(row=2, column= 5)
 
 # Sign in button 
-Button(text="Sign in", command= getvals).grid(row=7, column=5)
+Button(text="Sign in", command= login).grid(row=7, column=5)
 
 
 
